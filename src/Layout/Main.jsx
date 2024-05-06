1c340606946b3d527ece117a3351ba32f3fd5344
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-05-06 11:04:39 +0600

    add card

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-           <Outlet></Outlet> 
-           <Footer></Footer>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
